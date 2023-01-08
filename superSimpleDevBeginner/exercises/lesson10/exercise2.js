d54500a8043f0f9a2180ef98117780499585561c
Eric-windows <eric.babcock@protonmail.com>
2023-01-08 21:14:28 +0100

    finished lesson 10 exercises

--- superSimpleDevBeginner/exercises/lesson10/exercise2.js
+++ superSimpleDevBeginner/exercises/lesson10/exercise2.js
@@ -19,5 +19,56 @@ function cartTotal (cartArray) {
     return cartSum;
 }
 
+function receipt (sumOutput){
+    let receiptItem = document.createElement('div');
+    let receipt = document.getElementById("receipt");
+    receipt.innerText = 'Your cart total is ' + sumOutput;
+    document.body.append(receipt);
 
-cartTotal(myCart);
+
+
+}
+receipt(cartTotal(myCart));
+
+function clear() {
+    let receipt = document.getElementById("receipt");
+    receipt.innerText = '';
+
+}
+
+function lineItem(myCart) {
+    myCart.forEach( function (cartItem){
+        let receipt = document.getElementById("receipt");
+    
+        let quantity = parseInt(cartItem.quantity);
+        let price = parseInt(cartItem.price);
+        let lineItem = document.createElement('div');
+        
+        lineItem.innerText = cartItem.price + ' ' + cartItem.name + ' cost ' + quantity*price;
+        receipt.appendChild(lineItem)
+        
+    
+    });
+
+    
+}
+
+
+function addTotal(myCart) {
+    let sum = document.getElementById("receipt");
+    let total = document.createElement('div');
+
+    total.innerText = 'The Total of the cart is '+ cartTotal(myCart);
+    sum.appendChild(total);
+}
+
+
+
+
+function displayReceipt(myCart) {
+    clear();
+    lineItem(myCart);
+    addTotal(myCart);
+};
+
+displayReceipt(myCart);
