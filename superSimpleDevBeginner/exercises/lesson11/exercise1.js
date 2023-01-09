56565859a27b725c1c27f10030f913ace2c69e67
Eric-windows <eric.babcock@protonmail.com>
2023-01-09 21:01:47 +0100

    update exercise 10 assignments

--- superSimpleDevBeginner/exercises/lesson11/exercise1.js
+++ superSimpleDevBeginner/exercises/lesson11/exercise1.js
@@ -1,16 +1,33 @@
-cartItems = [{
-    name: 'Apple',
-    price: '3'
-},{
-    name:'Orange',
-    price:'4'
-},{
-    name:'egg',
-    price:'2'
-}];
+let total = 0;
 
-function createCart() {
-console.log('totalPrice');
-};
+function createCart(foodPrices) {
+  const foods = Object.keys(foodPrices);
 
-createCart();
+  foods.forEach(function (food) {
+    const cartItem = document.createElement('div');
+
+    // Here we get the food price using the food variable.
+    // If food = 'Apple', then foodPrices[food] is the same
+    // as foodPrices['Apple'], which is the same as foodPrices.Apple
+    const foodPrice = foodPrices[food];
+    cartItem.innerText = food + ' $' + foodPrice;
+
+    const addButton = document.createElement('button');
+    addButton.innerText = 'Add';
+
+    // Sorry I didn't teach this (I'll revise in the 2022 edition)!
+    // You'll have to use a function without a name here. If you
+    // create a separate addToCart() function, it will not have
+    // access to any of the variables in this function.
+    addButton.onclick = function () {
+      total = total + foodPrice;
+    };
+
+    cartItem.appendChild(addButton);
+
+    // This wil render each food with an "Add" button onto the webpage.
+    document.body.appendChild(cartItem);
+  });
+}
+
+createCart({ Apple: 3, Orange: 4, Egg: 2 });
---
+++ superSimpleDevBeginner/lessons/lesson12/rendering.js
@@ -0,0 +1,57 @@
+const todos = [{
+  title:"Get groceries",
+  dueDate: '20210-10=04',
+  id:'id1'
+
+  },{
+    title:"Wash car",
+    dueDate:"2021-02-03",
+    id:'id2'
+  },{
+    title:"Make dinner",
+    dueDate:"2021-02-03",
+    id:'id3'
+  }];
+
+render();
+function addTodo() {
+  const textbox = document.getElementById("todo-title");
+  const title = textbox.value;
+
+  const datePicker = document.getElementById('date-picker');
+  const id = new Date().getTime();
+  todos.push({
+    title:title,
+    dueDate:datePicker.value,
+    id: id
+  });
+  render();
+}
+
+function deleteTodo(event){
+  const deleteButton = event.target;
+  const idToDelete = deleteButton.id;
+
+
+
+};
+
+function render() {
+  // reset our list
+  document.getElementById("todo-list").innerHTML = "";
+
+  todos.forEach(function (todo) {
+    const element = document.createElement("div");
+    element.innerText = todo.title + ' ' + todo.dueDate;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.style = 'margin-left: 12px;';
+    deleteButton.onclick = deleteTodo;
+    deleteButton.id = todo.id;
+    element.appendChild(deleteButton);
+
+    const todoList = document.getElementById("todo-list");
+    todoList.appendChild(element);
+  });
+}
