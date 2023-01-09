b395f1f8661b5d21fa415b0d158ddd0b0e65be39
Eric-windows <eric.babcock@protonmail.com>
2023-01-09 17:03:41 +0100

    finished lesson 11

---
+++ superSimpleDevBeginner/exercises/lesson11/exercise1.js
@@ -0,0 +1,16 @@
+cartItems = [{
+    name: 'Apple',
+    price: '3'
+},{
+    name:'Orange',
+    price:'4'
+},{
+    name:'egg',
+    price:'2'
+}];
+
+function createCart() {
+console.log('totalPrice');
+};
+
+createCart();
