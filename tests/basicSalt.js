ae0fa064e898f8ef77f621b5400536a7b9622882
Eric Babcock <eric.babcock@protonmail.com>
2023-02-06 13:31:31 +0100

    Update basicSalt.js

--- tests/basicSalt.js
+++ tests/basicSalt.js
@@ -1,5 +1,7 @@
 const { scryptSync, randomBytes } = require('crypto');
 
+const users = [];
+
 function signup(email, password) {
     const salt = randomBytes(16).toString('hex');
     const hashedPassword = scryptSync(password, salt, 64);
@@ -19,5 +21,16 @@ function login(email, password) {
 
     const keyBuffer = Buffer.from (key, 'hex');
     const match = timingSafeEqual(hashedBuffer, keyBuffer);
-
 }
+console.log("hello")
+signup("bubs", "bubs3")
+signup("eric",'bubs3')
+
+console.log(users[0])
+
+login('bubs','bubsasdf')
+
+
+
+login('bubs','bubs3')
+
