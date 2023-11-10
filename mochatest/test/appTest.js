fce6feb735b6329ef8a0d2a22af9598fd748b268
eric babcock <babcockew@gmail.com>
2023-11-10 15:33:45 +0100

    added addNumbers function and tests

--- mochatest/test/appTest.js
+++ mochatest/test/appTest.js
@@ -5,10 +5,25 @@
 // We want Chai
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
-
+const addNumbers = require('../app').addNumbers;
 describe('App', function(){
-  it('app should return hello', function(){
+  it('function sayHello should return hello', function(){
     let result = sayHello();
     assert.equal(result,'hello');
+  });
+
+  it('function sayHello should return type string', function (){
+    let result = sayHello();
+    assert.typeOf(result,'string');
   })
-})
+
+  it('addNumbers should be above 5', function(){
+    let result = addNumbers(5,5);
+    assert.isAbove(result,5);
+  })
+
+  it('function addNumbers should return type number', function(){
+    let result = addNumbers(5,5);
+    assert.typeOf(result,'number')
+  })
+});
