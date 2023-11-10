7443f712ae814f9442e29c07c02d28ae8d3ce61c
eric babcock <babcockew@gmail.com>
2023-11-10 15:16:33 +0100

    first test passes

---
+++ mochatest/test/appTest.js
@@ -0,0 +1,13 @@
+/* standard js assertion library
+  const assert = require('assert').assert;
+ */
+
+// We want Chai
+const assert = require('chai').assert;
+const app = require('../app');
+
+describe('App', function(){
+  it('app should return hello', function(){
+    assert.equal(app(),'hello');
+  })
+})
