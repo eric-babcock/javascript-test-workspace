a92d00f43b25484a660aed51d849a497200fa4b0
Eric Babcock <eric.babcock@protonmail.com>
2024-07-11 08:55:08 +0200

    Update lesson1intro.js

--- superSimpleDevBeginner/exercises/lesson1/lesson1intro.js
+++ superSimpleDevBeginner/exercises/lesson1/lesson1intro.js
@@ -1,5 +1,27 @@
+1a. alert('Good morning!');
+
+1b. alert('Eric');
+
+1c. 10+5
+
+1d. 20-5
+
+1e. 2+2-5
+
+1f. document.body.innerHTML = 'Good morning!';
+
+1g. document.body.innerHTML = 'Eric'
+
+
+1h. order = {t_shirt: 10, socks: 8, dinner_plates: 20};
+order.t_shirt+order.socks+order.dinner_plates
+
+1i. 100-20-50+200
+
+
+
+
 alert('hello');
 
 console.log('hi')
 
-document.body.innerHTML = 'hello';
