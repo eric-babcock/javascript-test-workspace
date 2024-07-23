fcb93d09b822ac1db7a476b5f1a09a28ca6d05e6
Eric Babcock <eric.babcock@protonmail.com>
2024-07-23 08:31:02 +0200

    updated todo app

--- techEndeavor/react-todo-app/src/App.js
+++ techEndeavor/react-todo-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todos/Todo';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World</h1>
+      <Todo />
     </div>
   );
 }
