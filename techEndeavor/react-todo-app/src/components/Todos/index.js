ac6bd5c906b0c22abe23670871b0c8ce366fafcd
eric babcock <babcockew@gmail.com>
2024-08-01 09:13:50 +0200

    elevated todos to index

--- techEndeavor/react-todo-app/src/components/Todos/index.js
+++ techEndeavor/react-todo-app/src/components/Todos/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
+const todos = [
+  {id: 1, message: 'wallk the dog'},
+  {id: 2, message: 'take out trash'},
+  {id: 3, message: 'finish tutorial video'}
+]
+
 function Todos() {
   return(
     <>
