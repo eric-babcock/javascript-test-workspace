ce19ced216b6ecb5203e3c7305a04c1a465731e8
eric babcock <babcockew@gmail.com>
2024-09-24 22:02:36 +0200

    added redux actions to Counter

--- dave-ceddia/complete-redux/src/components/Counter.jsx
+++ dave-ceddia/complete-redux/src/components/Counter.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { INCREMENT, DECREMENT, RESET } from "../actions/actions";
 
 /*
 const letters = ['r', 'e', 'd', 'u', 'x'];
@@ -10,6 +12,12 @@ var word = letters.reduce(
 );
 */
 
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
 class Counter extends React.Component {
   // remove local state
   //state = { count: 42 }
@@ -20,15 +28,22 @@ class Counter extends React.Component {
     /*this.setState({
       count: this.state.count + 1
     });*/
-  }
+    // replace with
+    this.props.dispatch({ type: INCREMENT });
+  };
 
   decrement = () => {
     // and this
     /*this.setState({
       count: this.state.count -1
     });*/
+    // replace with 
+    this.props.dispatch({ type: DECREMENT });
   };
 
+  reset = () => {
+    this.props.dispatch({ type: RESET });
+  };
   
 
   /* `reduce` takes 2 arguments:
@@ -57,9 +72,12 @@ class Counter extends React.Component {
             {word}
           </h3>*/}
         </div>
+        <div className="reset">
+          <button onClick={this.reset}>Reset</button>
+        </div>
       </div>
     );
   }
 }
 
-export default Counter;
+export default connect(mapStateToProps)(Counter);
