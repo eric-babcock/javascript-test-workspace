d1cee11a3bd570d05e9d73a477963c63c0698854
Eric Babcock <babcockew@gmail.com>
2024-10-16 16:42:52 +0200

    working on display card

--- promise-tutorial-pokemon-freecodecamp/src/App.tsx
+++ promise-tutorial-pokemon-freecodecamp/src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import getPokemon from './helpers/getPokemon'
+import getPokemon from './components/pokemonCard/getPokemon'
+import PokemonCards from './components/pokemonCard/PokemonCards'
+import PokemonCard from './components/pokemonCard/PokemonCard'
 
 function App() {
 
   return (
     <>
+      <PokemonCard />
       <div className="card">
         <button onClick={() => getPokemon()}>
           count is
